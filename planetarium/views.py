6a3e2e0394822a64b7a18004693693b834478b2f
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-10 17:55:56 +0300

    Add blank line to user.urls

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+class ShowThemeViewSet:
+    pass
+
+
+class AstronomyShowViewSet:
+    pass
+
+
+class ShowSessionViewSet:
+    pass
+
+
+class PlanetarumDomeViewSet:
+    pass
+
+
+class TicketViewSet:
+    pass
+
+
+class ReservationViewSet:
+    pass
