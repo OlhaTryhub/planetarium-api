e3edafd5910664e3267ae13ba67c5beb66ce85cc
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-13 23:59:02 +0300

    Add tests for planetarium views

--- planetarium/tests.py
+++ planetarium/tests.py
@@ -3,9 +3,19 @@ from datetime import datetime
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils.timezone import make_aware
+
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
 
-from .models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Reservation, Ticket
+from .models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket
+)
 
 
 class ModelTestCase(TestCase):
@@ -77,3 +87,58 @@ class ModelTestCase(TestCase):
             Ticket.validate_ticket(
                 invalid_row, valid_seat, valid_dome, ValidationError
             )
+
+
+class ViewTestCase(APITestCase):
+    def setUp(self):
+        self.theme = ShowTheme.objects.create(name="Space Exploration")
+        self.show = AstronomyShow.objects.create(
+            title="Starry Night", description="A night sky display"
+        )
+        self.dome = PlanetariumDome.objects.create(
+            name="Dome A", rows=10, seats_in_row=12
+        )
+        show_time = make_aware(datetime(2023, 8, 14, 15, 30))
+        self.session = ShowSession.objects.create(
+            astronomy_show=self.show,
+            planetarium_dome=self.dome,
+            show_time=show_time
+        )
+        self.user = get_user_model().objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.reservation = Reservation.objects.create(user=self.user)
+        self.ticket = Ticket.objects.create(
+            row=5,
+            seat=3,
+            show_session=self.session,
+            reservation=self.reservation
+        )
+
+    def test_create_reservation(self):
+        url = "/api/planetarium/reservations/"
+        data = {
+            "user": self.user.id,
+            "tickets": [
+                {
+                    "row": 1,
+                    "seat": 1,
+                    "show_session": self.session.id
+                }
+            ]
+        }
+
+        client = APIClient()
+        client.force_authenticate(user=self.user)
+
+        response = client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_reservations(self):
+        url = "/api/planetarium/reservations/"
+
+        client = APIClient()
+        client.force_authenticate(user=self.user)
+
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
