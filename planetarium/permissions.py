be3084c4df6b05269f5ac7c20d1f69b7372ec3e0
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-24 20:48:00 +0300

    Update planetarium permission

--- planetarium/permissions.py
+++ planetarium/permissions.py
@@ -13,16 +13,11 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
         )
 
 
-class IsClientUserOrIsAdminReadOnly(BasePermission):
+class IsAdminOrIfAuthenticatedReadCreateDeleteOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_staff
-                and request.user.is_authenticated
-            )
-            or (request.method in SAFE_METHODS + ("POST",)
+            (request.user and request.user.is_staff)
+            or (request.method in SAFE_METHODS + ("POST", "DELETE")
                 and request.user
                 and request.user.is_authenticated
                 and not request.user.is_staff)
