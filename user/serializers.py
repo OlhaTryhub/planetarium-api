6ef0214d15b84325fd09def052bd62325ed168ee
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-24 20:44:38 +0300

    Remove unnessesary doc strings from user/serilizers.py

--- user/serializers.py
+++ user/serializers.py
@@ -18,11 +18,9 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
-        """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        """Update a user, set the password correctly and return it"""
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
         if password:
